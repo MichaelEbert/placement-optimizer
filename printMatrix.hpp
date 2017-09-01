53ab1b05bcaa8b22fa464bf5af2ee3e192980228
Michael Ebert <mebert@pacbell.net>
2017-09-01 12:36:36 -0700

    added printMatrix.hpp

---
+++ printMatrix.hpp
@@ -0,0 +1,87 @@
+#pragma once
+#include "types.hpp"
+template<typename T>
+int printMatrix(T* matrix) noexcept
+{
+	for(int i = 0; i < GRID_Y_SIZE; i++){
+		for(int j = 0; j < GRID_X_SIZE; j++){
+			printf("%d\t", matrix[cell_linear_offset(j,i)]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+template<>
+int printMatrix(float* matrix) noexcept
+{
+	printf("float\n");
+	for(int i = 0; i < GRID_Y_SIZE; i++){
+		for(int j = 0; j < GRID_X_SIZE; j++){
+			printf("%f\t", matrix[cell_linear_offset(j,i)]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+template<>
+int printMatrix(cell* matrix) noexcept
+{
+	char toPrint = 'x';
+	for(int i = 0; i < GRID_Y_SIZE; i++){
+		for(int j = 0; j < GRID_X_SIZE; j++){
+			switch(matrix[cell_linear_offset(j,i)]){
+				case NONE_ID:
+					toPrint = '_';
+					break;
+				case REACTOR_ID:
+					toPrint = 'R';
+					break;
+				case HEATSINK_ID:
+					toPrint = 'S';
+					break;
+				case SPREADER_ID:
+					toPrint = '+';
+					break;
+				case BOILER_ID:
+					toPrint = 'B';
+					break;
+				default:
+					toPrint = '0'+matrix[cell_linear_offset(j,i)];
+					break;
+			}
+			printf(" %c ", toPrint);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+template<>
+int printMatrix(LocalVars* matrix) noexcept
+{
+	printf("%d",sizeof(void*));
+	printf("netPtr:\n");
+	for(int i = 0; i < GRID_Y_SIZE; i++){
+		for(int j = 0; j < GRID_X_SIZE; j++){
+			printf("%X\t", matrix[cell_linear_offset(j,i)].netPtr);
+		}
+		printf("\n");
+	}
+	printf("localA:\n");
+	for(int i = 0; i < GRID_Y_SIZE; i++){
+		for(int j = 0; j < GRID_X_SIZE; j++){
+			printf("%d\t", matrix[cell_linear_offset(j,i)].localA);
+		}
+		printf("\n");
+	}
+	printf("localB:\n");
+	for(int i = 0; i < GRID_Y_SIZE; i++){
+		for(int j = 0; j < GRID_X_SIZE; j++){
+			printf("%d\t", matrix[cell_linear_offset(j,i)].localB);
+		}
+		printf("\n");
+	}
+	return 0;
+}
